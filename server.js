413c2e1dfbfdc75d9f3800d6cf40e775b61509d4
Dhanashri Bhavsar <129056367+dhanashri-code@users.noreply.github.com>
2025-02-20 19:44:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+    res.json("server start")
+})
+
 app.use(authroutes);
 app.use(toDoRoutes);
 
